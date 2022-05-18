00d582a44ba77c38d4bcdf53ebed45db350558ea
LeoZion <leozionsuporte@gmail.com>
2022-05-18 18:39:22 -0300

    add new role team member

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -63,5 +63,9 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('suport', 'Support', [
+            'read',
+        ])->description('Support users have the ability to only read.');
     }
 }
